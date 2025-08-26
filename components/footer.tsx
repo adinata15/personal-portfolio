fddc73f35f8d14a42374a2c4efacaaac7f4863f1
adinata15 <adinatatan99@gmail.com>
2025-08-27 01:11:30 +0800

    Standardise portrait design

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
         <footer className="bg-gray-800 dark:bg-gray-900 text-white py-8 px-8">
             <div className="max-w-6xl mx-auto text-center">
                 <div className="flex items-center justify-center space-x-3 mb-4">
-                    <div className="w-8 h-8 min-w-8 bg-orange-300 rounded-full flex items-center justify-center overflow-hidden">
+                    <div className="w-8 h-8 min-w-8 bg-gray-200 border-2 border-orange-400 rounded-full flex items-center justify-center overflow-hidden">
                         <Image
                             src="/images/personal.png"
                             alt="Personal Portrait"
